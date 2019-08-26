c39076378b56897848d7896625d15b4d36b12996
milano12345 <dmilano857@gmail.com>
2019-08-26 17:48:08 -0500

    created cards for data

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import StrainCards from './Components/StrainCards'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <StrainCards/>
       </header>
     </div>
   );
